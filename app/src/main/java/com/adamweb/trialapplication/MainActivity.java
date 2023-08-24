8c5679046ece8040bbace1d2da662411dda2bb8c
Real_Adamweb <117515903+Adamweb-jido@users.noreply.github.com>
2023-08-24 13:03:57 +0100

    An App for my College

--- app/src/main/java/com/adamweb/trialapplication/MainActivity.java
+++ app/src/main/java/com/adamweb/trialapplication/MainActivity.java
@@ -18,12 +18,8 @@ public class MainActivity extends AppCompatActivity {
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
-       this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-      //  setTheme(R.style.Theme_TrialApplication);
-        
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
     }
 }
