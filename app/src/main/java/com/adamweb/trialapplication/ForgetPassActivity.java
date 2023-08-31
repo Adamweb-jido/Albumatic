9360edd38efa82f74599e6ace6479126d7be6a4a
Real_Adamweb <117515903+Adamweb-jido@users.noreply.github.com>
2023-08-31 12:56:53 +0100

    An App for my College

--- app/src/main/java/com/adamweb/trialapplication/ForgetPassActivity.java
+++ app/src/main/java/com/adamweb/trialapplication/ForgetPassActivity.java
@@ -2,7 +2,10 @@ package com.adamweb.trialapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ForgetPassActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class ForgetPassActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_pass);
+        TextView textView = (TextView) findViewById(R.id.loginLink);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
