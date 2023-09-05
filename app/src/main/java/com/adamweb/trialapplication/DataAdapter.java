8ab10c593455df2490b373ec9449513c8c43daee
Real_Adamweb <117515903+Adamweb-jido@users.noreply.github.com>
2023-09-05 21:51:56 +0100

    An App for my College

--- app/src/main/java/com/adamweb/trialapplication/DataAdapter.java
+++ app/src/main/java/com/adamweb/trialapplication/DataAdapter.java
@@ -1,24 +1,42 @@
 package com.adamweb.trialapplication;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class DataAdapter extends RecyclerView.Adapter<DataViewHolder> {
+
+    Context context;
+    List<DataItem> items;
+
+    public DataAdapter(Context context, List<DataItem> items) {
+        this.context = context;
+        this.items = items;
+    }
+
+
     @NonNull
     @Override
     public DataViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new DataViewHolder(LayoutInflater.from(context).inflate(R.layout.album_layout, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull DataViewHolder holder, int position) {
-
+        holder.name.setText(items.get(position).getName());
+        holder.admNo.setText(items.get(position).getAdmissionNumber());
+        holder.comb.setText(items.get(position).getCombination());
+        holder.phoneNo.setText(items.get(position).getPhoneNumber());
+        holder.coverImage.setImageResource(items.get(position).getImage());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.size();
     }
 }
--- app/src/main/java/com/adamweb/trialapplication/PeopleActivity.java
+++ app/src/main/java/com/adamweb/trialapplication/PeopleActivity.java
@@ -1,16 +1,22 @@
 package com.adamweb.trialapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PeopleActivity extends AppCompatActivity {
 
 
     ImageView imageView;
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +29,43 @@ public class PeopleActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+     List<DataItem> items = new ArrayList<DataItem>();
+     items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+        items.add(new DataItem("Adam Ibrahim Yau", "Csc/01/19/0001", "Com/Chem", 2, R.drawable.photoalbum));
+
+        recyclerView = findViewById(R.id.albumRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new DataAdapter(getApplicationContext(), items));
     }
 }
