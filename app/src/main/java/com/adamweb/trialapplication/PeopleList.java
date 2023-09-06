f6ce43872d443a1d8a41a8ba7e09e78a69ffd067
Real_Adamweb <117515903+Adamweb-jido@users.noreply.github.com>
2023-09-06 19:30:27 +0100

    An App for my College

--- app/src/main/java/com/adamweb/trialapplication/PeopleList.java
+++ app/src/main/java/com/adamweb/trialapplication/PeopleList.java
@@ -1,14 +1,20 @@
 package com.adamweb.trialapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PeopleList extends AppCompatActivity {
         ImageView imageView;
+        RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +27,34 @@ public class PeopleList extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        List<ItemPeopleList> items = new ArrayList<>();
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
+        items.add(new ItemPeopleList("Adamu Ibrahim Ya'u", "Realadamweb@gmail.com",R.drawable.clipartprofile));
 
+        recyclerView = findViewById(R.id.listPeople);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new ListAdapter(getApplicationContext(), items));
     }
 }
