ba5979711a6b85dcf6b3802399302f0831f227ba
Real_Adamweb <117515903+Adamweb-jido@users.noreply.github.com>
2023-09-21 14:44:03 +0100

    x

--- app/src/main/java/com/adamweb/trialapplication/LoginActivity.java
+++ app/src/main/java/com/adamweb/trialapplication/LoginActivity.java
@@ -9,16 +9,12 @@ import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
      TextView forget, register;
-    public void myButton(View v){
-        Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
-        startActivity(intent);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-     forget = (TextView) findViewById(R.id.forgetPassword);
+     forget = findViewById(R.id.forgetPassword);
      forget.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View view) {
