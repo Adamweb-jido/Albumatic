7712b90b7cb9eeb341c388fd9f6c01aa2b710be8
REAL_ADAMWEB <realadamweb@gmail.com>
2023-10-24 00:56:03 +0100

    continue

--- app/src/main/java/com/adamweb/trialapplication/LoginActivity.java
+++ app/src/main/java/com/adamweb/trialapplication/LoginActivity.java
@@ -1,27 +1,14 @@
 package com.adamweb.trialapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
-     TextView forget, register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-     forget = findViewById(R.id.forgetPassword);
-     forget.setOnClickListener(view -> {
-         Intent intent = new Intent(getApplicationContext(), ForgetPassActivity.class);
-         startActivity(intent);
-     });
-     register = findViewById(R.id.register);
-     register.setOnClickListener(view -> {
-         Intent intent = (new Intent(getApplicationContext(), SignUpActivity.class));
-         startActivity(intent);
-     });
     }
 }
