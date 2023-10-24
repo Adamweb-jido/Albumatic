41e5c81d4f55a892bbe71f73c7aca6995cfe080f
REAL_ADAMWEB <realadamweb@gmail.com>
2023-10-24 01:05:54 +0100

    continue

--- app/src/main/java/com/adamweb/trialapplication/LoginActivity.java
+++ app/src/main/java/com/adamweb/trialapplication/LoginActivity.java
@@ -1,14 +1,29 @@
 package com.adamweb.trialapplication;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.button.MaterialButton;
+
 public class LoginActivity extends AppCompatActivity {
 
+    MaterialButton button;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        button = findViewById(R.id.myButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
